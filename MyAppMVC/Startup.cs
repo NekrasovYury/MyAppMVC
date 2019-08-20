2c28984b8e4bcb2ae1acd03156f4dbf8d798cc3c
Furynoobas <samsung12345#>
2019-08-20 12:55:46 +0300

    Add project files.

---
+++ MyAppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAppMVC.Startup))]
+namespace MyAppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
